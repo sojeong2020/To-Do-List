26e577963958972d686c234490aab9ea4ee64448
Sojeong Park <macpark09@gmail.com>
2021-01-14 23:57:53 +0000

    modify todo.js

--- todo.js
+++ todo.js
@@ -1,6 +1,17 @@
-let input = prompt("What wouldyou like to do?")
-while (input !== 'quit') {
-    input = prompt("What would you like to do?")
+let input = prompt("What wouldyou like to do?");
+const todos = ['buy milk', 'clean the room'];
+while (input !== 'quit' && input !== 'q') {
+   if (input === 'list') {
+       console.log('*************')
+       for (let i = 0; i < todos.length; i++) {
+           console.log(`${i}: ${todos[i]}`);
+       }
+       console.log('************')
+    }  else if (input ==='new') {
+        const newTODO = prompt('OK, what is the new todo?');
+        todos.push(newTODO);
+        console.log(`${newTODO} added to the list!`)
+    }
 }
 console.log("OK QUIT THE APP!")
 
