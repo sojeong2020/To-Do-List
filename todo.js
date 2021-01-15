f7eab2bb268f43ce626c3dc0e167be85a206c6c9
Sojeong Park <macpark09@gmail.com>
2021-01-15 01:14:19 +0000

    modify todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-let input = prompt("What wouldyou like to do?");
+let input = prompt("What would you like to do?");
 const todos = ['buy milk', 'clean the room'];
 while (input !== 'quit' && input !== 'q') {
    if (input === 'list') {
@@ -7,11 +7,20 @@ while (input !== 'quit' && input !== 'q') {
            console.log(`${i}: ${todos[i]}`);
        }
        console.log('************')
-    }  else if (input ==='new') {
+    } else if (input ==='new') {
         const newTODO = prompt('OK, what is the new todo?');
         todos.push(newTODO);
         console.log(`${newTODO} added to the list!`)
+    } else if (input ==='delete') {
+        const index = parseInt(prompt('OK, enter an index to delete;'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log('OK, deleted ${deleted[0]}');
+        } else {
+            console.log('Unknown index')
+        }
     }
+    input = prompt('What would you like to do?')
 }
 console.log("OK QUIT THE APP!")
 
